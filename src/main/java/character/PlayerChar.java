2e1caa227191a51dddd3f38ef7d67ddcc839d2a7
RafaelRResnauer <resnauer.rafael@gmail.com>
2021-02-15 22:11:57 -0300

    Initial character development

---
+++ src/main/java/character/PlayerChar.java
@@ -0,0 +1,70 @@
+package character;
+
+public class PlayerChar {
+    private String name;
+    private Race race;
+    private CharClass charClass;
+    private Background background;
+    private Alignment alignment;
+    private String playerName;
+    private int xp;
+
+    private int strength;
+    private int strengthMod;
+    private int dexterity;
+    private int dexterityMod;
+    private int constitution;
+    private int constitutionMod;
+    private int intelligence;
+    private int intelligenceMod;
+    private int wisdom;
+    private int wisdomMod;
+    private int charisma;
+    private int charismaMod;
+
+    private boolean inspiration;
+    private int proficiencyBonus;
+
+    private int strengthSave;
+    private int dexteritySave;
+    private int constitutionSave;
+    private int intelligenceSave;
+    private int wisdomSave;
+    private int charismaSave;
+
+    private int acrobatics;
+    private int animalHandling;
+    private int arcana;
+    private int athletics;
+    private int deception;
+    private int history;
+    private int insight;
+    private int intimidation;
+    private int investigation;
+    private int medicine;
+    private int nature;
+    private int perception;
+    private int performance;
+    private int persuasion;
+    private int religion;
+    private int sleightOfHand;
+    private int stealth;
+    private int survival;
+    private int passiveWisdom;
+
+    private int ac;
+    private int initiative;
+    private int speed;
+
+    private int hpTotal;
+    private int hpCurrent;
+    private int hpTemp;
+
+    private String hitDiceType;
+    private int hitDiceTotal;
+    private int hitDiceCurrent;
+
+
+
+
+}
